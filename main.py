1913e939d249911dff602b5e38dc15e102bf750e
misilelab <misileminecord@gmail.com>
2025-05-04 20:39:16 +0900

    fix: missing router import
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ from contextlib import asynccontextmanager
 from libraries.initalizer import scheduler
 from libraries.arxiv import refresh_cache
 
+from routers.oauth import router as oauth_router
+from routers.user import router as user_router
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5173"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
+app.include_router(oauth_router)
+app.include_router(user_router)
 
 @asynccontextmanager
 async def on_start():
