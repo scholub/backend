7bb973bd121a68a35d984729e8d86c24d4d59817
misilelaboratory <misileminecord@gmail.com>
2025-05-04 21:13:29 +0900

    fix(email): boto3 to smtp to fix email
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- libraries/email.py
+++ libraries/email.py
@@ -1,26 +1,24 @@
 from os import getenv
-
-from boto3 import client # pyright: ignore[reportMissingTypeStubs, reportUnknownVariableType]
-
-email = client( # pyright: ignore[reportUnknownVariableType]
-  "ses",
-  aws_access_key_id = getenv("AWS_SERVER_PUBLIC_KEY", ""),
-  aws_secret_access_key = getenv("AWS_SERVER_SECRET_KEY", ""),
-  region_name = getenv("AWS_SERVER_REGION", "eu-west-2")
-)
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def send_email(title: str, body: str, receiver: list[str]):
+  smtp_host = getenv("AWS_SES_SMTP_HOST", "email-smtp.eu-west-2.amazonaws.com")
+  smtp_port = int(getenv("AWS_SES_SMTP_PORT", "587"))
+  smtp_user = getenv("AWS_SERVER_PUBLIC_KEY", "")
+  smtp_pass = getenv("AWS_SERVER_SECRET_KEY", "")
+  sender = getenv("EMAIL_SENDER", "Scholub <scholub@schale.misile.xyz>")
+
   for i in receiver:
-    email.send_email( # pyright: ignore[reportUnknownMemberType]
-      Destination={
-        'ToAddresses': [i]
-      },
-      Message = {
-        'Body': {
-          'Html': {'Charset': 'UTF-8', 'Data': body}
-        },
-        'Subject': {'Charset': 'UTF-8', 'Data': title}
-      },
-      Source = getenv("EMAIL_SENDER", "Scholub <scholub@schale.misile.xyz>")
-    )
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = i
+    msg["Subject"] = title
+    msg.attach(MIMEText(body, "html", "utf-8"))
+
+    with smtplib.SMTP(smtp_host, smtp_port) as server:
+      _ = server.starttls()
+      _ = server.login(smtp_user, smtp_pass)
+      _ = server.sendmail(sender, i, msg.as_string())
 
