bf66605159148a867a62b15d4f6b8b996c5ca692
misilelab <misileminecord@gmail.com>
2025-05-23 10:03:24 +0900

    feat(paper): download recent papers
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- libraries/paper.py
+++ libraries/paper.py
@@ -1,13 +1,32 @@
+from datetime import datetime
 from os.path import getmtime
 from pathlib import Path
 
-from arxiv import Client, Search  # pyright: ignore[reportMissingTypeStubs]
+from arxiv import (  # pyright: ignore[reportMissingTypeStubs]
+  Client,
+  Search,
+  SortCriterion,
+)
 
 from libraries.initalizer import (
   get_data_path,
 )
 
 client = Client()
+slow_client = Client(page_size=1000, num_retries=5, delay_seconds=10)
+
+async def download_recent_posts():
+  result_generator = slow_client.results(
+    Search(
+      query = "cat:cs.AI",
+      sort_by = SortCriterion.SubmittedDate
+    )
+  )
+  result = next(result_generator)
+  now = datetime.now().date()
+  while result.published < now:
+    yield download_arxiv(result.entry_id)
+    result = next(result_generator)
 
 async def download_arxiv(paper_id: str, force: bool = False) -> Path:
   paper = next(client.results(
@@ -23,7 +42,6 @@ async def download_arxiv(paper_id: str, force: bool = False) -> Path:
 async def refresh_cache():
   papers = list(get_data_path("cache").glob("*.pdf"))
   paper_ids = [i.stem for i in papers]
-  slow_client = Client(num_retries=5, delay_seconds=10)
   for paper, paper_cached in zip(slow_client.results(Search(id_list=paper_ids)), papers):
     if paper.updated.timestamp() > getmtime(paper_cached):
       paper_cached.unlink()
