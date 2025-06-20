83e29c6fc9b0d28345a5580bc28446c0a42107b1
misilelab <misileminecord@gmail.com>
2025-06-20 20:55:21 +0900

    feat(posts): make start_date, end_date optional
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- routers/posts.py
+++ routers/posts.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 
 from fastapi import APIRouter
 
@@ -28,9 +28,15 @@ async def recommend_posts(user: cookieDep) -> list[GetRecommendsResultRecommends
 
 @router.get("/like")
 async def posts_like(
-  start_date: datetime,
-  end_date: datetime
+  start_date: datetime | None = None,
+  end_date: datetime | None = None
 ) -> list[GetPostsLikeOrderResult]:
+  if start_date is None:
+    today = datetime.now().date()
+    start_date = datetime.combine(today, time.min)
+  if end_date is None:
+    today = datetime.now().date()
+    end_date = datetime.combine(today, time.max)
   return await get_posts_like_order(db, start_date=start_date, end_date=end_date)
 
 
